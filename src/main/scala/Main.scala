355bf1d0ad30ab13d1a89b42a65d25cc69c3d9ec
Liyeuk Reynald <reynaldjoabet@gmail.com>
2024-06-14 22:44:57 +0400

    Formatted files

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,23 @@
-import cats.effect.ExitCode
 import cats.effect._
-import org.http4s.ember.server.EmberServerBuilder
+import cats.effect.std.Random
+import cats.effect.ExitCode
+
 import com.comcast.ip4s._
+import io.circe
+import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.defaults.Banner
+import org.http4s.server.middleware.RequestLogger
 import org.http4s.server.Server
 import org.typelevel.log4cats.{Logger, SelfAwareStructuredLogger}
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import org.typelevel.log4cats.LoggerName
-import io.circe
 import routes._
 import services._
-import org.http4s.server.middleware.RequestLogger
-import cats.effect.std.Random
+
 object Main extends IOApp {
+
 //implicit val loggerName=LoggerName("name")
-  private implicit val logger = Slf4jLogger.getLogger[IO]
+  implicit private val logger = Slf4jLogger.getLogger[IO]
 
   private def showEmberBanner[F[_]: Logger](s: Server): F[Unit] =
     Logger[F].info(
